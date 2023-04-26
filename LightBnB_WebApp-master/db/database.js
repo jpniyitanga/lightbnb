ff4c5f8b48b0b30c4b8e490b81c8a137fd61c44f
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-04-25 23:14:24 -0400

    Implement getUserWithId

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -37,7 +37,15 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1`, [id])
+    .then((result) => {
+      console.log("User with id", result.rows[0]);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
 };
 
 /**
